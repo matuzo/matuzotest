eca17e2a333bfb0622447d56837762f8d0841711
Manuel Matuzovic <manuel@matuzo.at>
2022-02-11 11:23:34 +0100

    Remove query-string

--- netlify/functions/log.js
+++ netlify/functions/log.js
@@ -2,9 +2,7 @@ exports.handler = async function (event, context) {
   const fs = require('fs')
   const queryString = require('query-string');
   
-  const parsed = queryString.parse(location.search);
-
-  fs.writeFile('/idlog.txt', parsed.q, err => {
+  fs.writeFile('/idlog.txt', event.queryStringParameters.q, err => {
     if (err) {
       console.error(err)
       return
